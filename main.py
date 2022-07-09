699402041f6f4d06c80a7defe1c281e0912c7338
Linus <linus.fraundorfer@enrag.at>
2022-07-09 20:13:46 +0200

    pesky comparison is done, Im missing face-to-face one

--- main.py
+++ main.py
@@ -17,24 +17,47 @@ from factor_analyzer import FactorAnalyzer
 
 ##modifications on data files, needed initially, kept until end though
 df1= pd.read_csv("data_gaeng.csv")
-# df2= pd.read_csv("data_ga.csv")
-# df3= pd.read_csv("data_noga.csv") 
+df2= pd.read_csv("data_ga.csv")
+df3= pd.read_csv("data_noga.csv") 
 
 cols1 = df1.columns.tolist()
-cols1
-lista = (9,8,7)
-#remove zeitstempel = 0
+cols2 = df2.columns.tolist()
+cols3 = df2.columns.tolist()
 df1.columns
 ###da wo 50% steht = 15
-myorder = [1, 3, 4, 6, 2, 7, 8, 19, 9, 15, 16, 17, 18, 45, 38, 
+myorder1 = [1, 3, 4, 6, 2, 7, 8, 19, 9, 15, 16, 17, 18, 45, 38, 
            44, 40, 41, 42, 43, 13, 14, 0, 11, 10,
             12, 20, 21, 22, 23, 31, 26, 24, 25, 27, 29, 28,
             30, 32, 33, 34, 35, 36, 37, 39, 51, 52, 53, 54,
             50, 47, 48, 49, 46] 
+
+myorder2 = [1,5,6,4,2,7,8,19,9,15,16, 17, 18, 45, 38, 
+           44, 40, 41, 42, 43, 13, 14, 0, 11, 10,
+            12, 20, 21, 22, 23, 31, 26, 24, 25, 27, 29, 28,
+            30, 32, 33, 34, 35, 36, 37, 39, 51, 52, 53, 54,
+            50, 47, 48, 49, 46]
+
+myorder3 = [1,5,6,4,2,7,8,19,9,15,16, 17, 18, 45, 38, 
+           44, 40, 41, 42, 43, 13, 14, 0, 11, 10,
+            12, 20, 21, 22, 23, 31, 26, 24, 25, 27, 29, 28,
+            30, 32, 33, 34, 35, 36, 37, 39, 51, 52, 53, 54,
+            50, 47, 48, 49, 46]
+
+
+
 #checkd bis 32 in new list
 
  # HIER 0]#, 1, 4]
-list1 = [cols1[i] for i in myorder]
+list1 = [cols1[i] for i in myorder1]
+df1 = df1[list1]
+
+list2 = [cols2[i] for i in myorder2]
+df2 = df2[list2]
+
+list3 = [cols3[i] for i in myorder3]
+df3 = df3[list3]
+
+
 
 #read in, snip, add together, remove that one header line (or do manually before), then save new name. ez
 #then add with noga
